93ad4d70b042760bef4b455386834da41b441502
Luis Fernando A. Lima <90354100+LuisFernandoALima@users.noreply.github.com>
2024-01-18 20:31:52 -0300

    Conclusão da área "Conhecimentos" (PC)

--- assets/js/caroussel.js
+++ assets/js/caroussel.js
@@ -1,31 +1,17 @@
 const $slideItem = document.querySelectorAll('.swiper-slide')
-const $prevButtom = document.querySelector('.swiper-button-prev')
-const $nextButtom = document.querySelector('.swiper-button-next')
 
 const $infoText = document.querySelectorAll('.info-text')
-var x = 0
 
-
-const eraseText = () => {
+const changeText = () => {
     $infoText.forEach(text => {
         text.classList.add('disable')
     });
-}
-
-const showText = () => {
     $slideItem.forEach((slide, index) => {
-        if (slide.classList.contains('swiper-slide-visible')) {
+        if (slide.classList.contains('swiper-slide-active')) {
             $infoText[index].classList.remove('disable');
         }
     });
 }
 
 
-const changeText = (direction) => {
-    eraseText()
-    showText()
-}
-
-$nextButtom.addEventListener('click', () => setTimeout(() => changeText('next'), 100));
-$prevButtom.addEventListener('click', () => setTimeout(() => changeText('prev'), 100));
-
+setInterval(changeText, 100)
