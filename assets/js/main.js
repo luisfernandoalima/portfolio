e7214ef9051e3237cc8cd13189748598b4ec6b02
Luis Fernando A. Lima <90354100+LuisFernandoALima@users.noreply.github.com>
2024-02-18 21:54:34 -0300

    Finalização do footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,26 @@ const X = document.querySelector('.fa-solid.fa-x');
 X.addEventListener('click', function() {
     removerPopUp();
 });
+
+// Footer
+document.querySelector('.meu-email').addEventListener('click', function() {
+    // Seleciona o elemento de texto
+    const texto = document.querySelector('.meu-email');
+
+    // Cria uma seleção
+    const selecion = window.getSelection();
+    const intervalo = document.createRange();
+
+    // Seleciona o conteúdo do elemento de texto
+    intervalo.selectNodeContents(texto);
+    selecion.removeAllRanges();
+    selecion.addRange(intervalo);
+
+    // Executa o comando de cópia
+    document.execCommand('copy');
+
+    // Remove a seleção para evitar realces indesejados
+    selecion.removeAllRanges();
+
+    alert("Email copiado!")
+});
